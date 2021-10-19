c1e11b5ee8600c523b567b0eaa6e798884a329a9
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-10-19 00:21:31 -0300

    add: Fetch Images

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,26 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const {isOpen, onClose, onOpen} = useDisclosure()
   // TODO SELECTED IMAGE URL STATE
-
+  const [selectedUrl, setSelectedUrl] = useState('')
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleSelectedImage(imageUrl: string){
+    setSelectedUrl(imageUrl)
+    onOpen()
+  }
 
   return (
     <>
       {/* TODO CARD GRID */}
-
+      <SimpleGrid columns={3} spacing={"40px"}>
+        {cards.map((card) => (
+          <Card data={card} key={card.id} viewImage={()=>{handleSelectedImage(card.url)}}/>
+        ))}
+      </SimpleGrid>
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage imgUrl={selectedUrl} isOpen={isOpen} onClose={onClose} />
+
     </>
   );
 }
--- src/pages/api/images.ts
+++ src/pages/api/images.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fauna from 'faunadb';
 
 const { query } = fauna;
+
 const client = new fauna.Client({ secret: process.env.FAUNA_API_KEY });
 
 interface ImagesQueryResponse {
